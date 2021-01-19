35204aa91e2e83ccfcded7c476bdee0277ae8257
jmenguitoo <joaquin.menguito080196@my.lsgh.edu.ph>
2021-01-19 09:51:07 +0800

    1 file modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -20,7 +20,7 @@ export class AboutPage {
   }
 
 calculateIdealWeight(){
-    if (this.gender = "m" && this.feet >= 5 && this.inches <= 11) {
+    if ((this.feet >= 5) && (this.inches <= 11) && (this.gender === "m")) {
       this.robinsonFormula = 52 + 1.9 * ((this.feet - 5) + this.inches);
        this.robinsonFormula = parseFloat(this.robinsonFormula.toFixed(0)) + " kg.";
       this.millerFormula = 56.2 + 1.41 * ((this.feet - 5) + this.inches);
@@ -28,8 +28,9 @@ calculateIdealWeight(){
       this.hamwiFormula = 48 + 2.7  * ((this.feet - 5) + this.inches);
        this.hamwiFormula = parseFloat(this.hamwiFormula.toFixed(0)) + " kg.";
       this.devineFormula = 50 + 2.3  * ((this.feet - 5) + this.inches);
-      this.devineFormula = parseFloat(this.devineFormula.toFixed(0)) + " kg.";
-    } else if (this.gender = "f" && this.feet >= 5 && this.inches <= 11) {
+       this.devineFormula = parseFloat(this.devineFormula.toFixed(0)) + " kg.";
+    } 
+      else if ((this.feet >= 5) && (this.inches <= 11) && (this.gender === "f")) {
       this.robinsonFormula = 49 + 1.7 * ((this.feet - 5) + this.inches);
        this.robinsonFormula = parseFloat(this.robinsonFormula.toFixed(0)) + " kg.";
       this.millerFormula = 53.1 + 1.36 * ((this.feet - 5) + this.inches);
@@ -38,11 +39,12 @@ calculateIdealWeight(){
        this.hamwiFormula = parseFloat(this.hamwiFormula.toFixed(0)) + " kg."; 
       this.devineFormula = 45.5 + 2.3  * ((this.feet - 5) + this.inches);
        this.devineFormula = parseFloat(this.devineFormula.toFixed(0)) + " kg."; 
-    } else {
-      this.robinsonFormula = "ERROR";
-      this.millerFormula = "ERROR";
-      this.hamwiFormula = "ERROR";
-      this.devineFormula = "ERROR";
+    } 
+      else {
+      this.robinsonFormula = "*ERROR*";
+      this.millerFormula = "*ERROR*";
+      this.hamwiFormula = "*ERROR*";
+      this.devineFormula = "*ERROR*";
     }
 }
 }
