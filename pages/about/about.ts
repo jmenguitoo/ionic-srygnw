ee8d9d9aba9d0b0f29c0ce51b0720dbf4597be8c
jmenguitoo <joaquin.menguito080196@my.lsgh.edu.ph>
2021-01-21 22:09:09 +0800

    1 file modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -21,23 +21,23 @@ export class AboutPage {
 
 calculateIdealWeight(){
     if ((this.feet >= 5) && (0 < this.inches <= 11) && (this.gender === "m")) {
-      this.robinsonFormula = 52 + 1.9 * ((this.feet - 5) + this.inches);
+      this.robinsonFormula = 52 + (1.9 * ((this.feet - 5)*12) + this.inches * 1.9);
        this.robinsonFormula = parseFloat(this.robinsonFormula.toFixed(0)) + " kg.";
-      this.millerFormula = 56.2 + 1.41 * ((this.feet - 5) + this.inches);
+      this.millerFormula = 56.2 + (1.41 * ((this.feet - 5)*12) + this.inches * 1.41);
        this.millerFormula = parseFloat(this.millerFormula.toFixed(0)) + " kg.";
-      this.hamwiFormula = 48 + 2.7  * ((this.feet - 5) + this.inches);
+      this.hamwiFormula = 48 + (2.7  * ((this.feet - 5)*12) + this.inches * 2.7);
        this.hamwiFormula = parseFloat(this.hamwiFormula.toFixed(0)) + " kg.";
-      this.devineFormula = 50 + 2.3  * ((this.feet - 5) + this.inches);
+      this.devineFormula = 50 + (2.3  * ((this.feet - 5)*12) + this.inches * 2.3);
        this.devineFormula = parseFloat(this.devineFormula.toFixed(0)) + " kg.";
     } 
       else if ((this.feet >= 5) && (0 < this.inches <= 11) && (this.gender === "f")) {
-      this.robinsonFormula = 49 + 1.7 * ((this.feet - 5) + this.inches);
+      this.robinsonFormula = 49 + (1.7 * ((this.feet - 5)*12) + this.inches * 1.7);
        this.robinsonFormula = parseFloat(this.robinsonFormula.toFixed(0)) + " kg.";
-      this.millerFormula = 53.1 + 1.36 * ((this.feet - 5) + this.inches);
+      this.millerFormula = 53.1 + (1.36 * ((this.feet - 5)*12) + this.inches * 1.36);
        this.millerFormula = parseFloat(this.millerFormula.toFixed(0)) + " kg."; 
-      this.hamwiFormula = 45.5 + 2.2  * ((this.feet - 5) + this.inches);
+      this.hamwiFormula = 45.5 + (2.2  * ((this.feet - 5)*12) + this.inches * 2.2);
        this.hamwiFormula = parseFloat(this.hamwiFormula.toFixed(0)) + " kg."; 
-      this.devineFormula = 45.5 + 2.3  * ((this.feet - 5) + this.inches);
+      this.devineFormula = 45.5 + (2.3  * ((this.feet - 5)*12) + this.inches * 2.3);
        this.devineFormula = parseFloat(this.devineFormula.toFixed(0)) + " kg."; 
     } 
       else {
